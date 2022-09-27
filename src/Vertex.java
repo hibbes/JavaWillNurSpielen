aeb50c43e989751f7405fe405603a8580dc0b80e
hibbes <marek@czernohous.de>
2022-09-27 07:45:06 +0200

    Master

---
+++ src/Vertex.java
@@ -0,0 +1,21 @@
+public class Vertex {
+    
+    public double x;
+    public double y;
+
+    public Vertex(double x, double y){
+        this.x=x;
+        this.y=y;
+    }
+     
+    public String toString() {
+        return "("+x+","+y+")";
+    }
+
+    public double length(){
+        return Math.sqrt(x*x+y*y);
+    }
+
+}
+
+
