ad42d891e4dde075a72eb15667a7a6ae4e60b5eb
hibbes <marek@czernohous.de>
2022-09-27 07:51:23 +0200

    Skalar

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,21 +1,31 @@
 public class Vertex {
-    
+
     public double x;
     public double y;
 
-    public Vertex(double x, double y){
-        this.x=x;
-        this.y=y;
+    public Vertex(double x, double y) {
+        this.x = x;
+        this.y = y;
     }
-     
+
     public String toString() {
-        return "("+x+","+y+")";
+        return "(" + x + "," + y + ")";
     }
 
-    public double length(){
-        return Math.sqrt(x*x+y*y);
+    public double length() {
+        return Math.sqrt(x * x + y * y);
     }
 
+    public void skalarMod(double s) {
+        this.x = x * s;
+        this.y = y.s;
+    }
+
+    public Vertex skalarMultMod(double s) {
+        return new Vertex(x * s, y * s);
+    }
+
+    public void addMod(Vertex v2) {
+        this.x = x + v2.x;
+    }
 }
-
-
