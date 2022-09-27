b96dc97cbb60f605f7a08b8ede038558ae05dc64
Marek Czernohous <m.czernohous@schiller-offenburg.de>
2022-09-27 07:45:06 +0200

    Master

---
+++ src/Test.java
@@ -0,0 +1,20 @@
+public class Test {
+    
+    public static void main(String args[]){
+
+        Vertex v1 = new Vertex(34,2);
+        Vertex v2 = new Vertex(42,0);
+        Vertex vv = new Vertex(0,0);
+
+
+
+
+        System.out.println(v1);
+        System.out.println(v2.toString());
+        System.out.println("v1.x = "+v1.x);
+
+        System.out.println(v1.length());
+
+    
+    }
+}
