f3c6368c068b2f8bea52bd116fda6ebe6a1d7447
Marek Czernohous <m.czernohous@schiller-offenburg.de>
2022-10-04 07:52:01 +0200

    SimpleTriangle

--- src/Test.java
+++ src/Test.java
@@ -1,20 +1,24 @@
 public class Test {
-    
-    public static void main(String args[]){
 
-        Vertex v1 = new Vertex(34,2);
-        Vertex v2 = new Vertex(42,0);
-        Vertex vv = new Vertex(0,0);
+    public static void main(String args[]) {
 
-
-
+        Vertex v1 = new Vertex(34, 2);
+        Vertex v2 = new Vertex(42, 0);
+        Vertex v4 = new Vertex(34, 2);
+        Vertex vv = new Vertex(0, 0);
 
         System.out.println(v1);
         System.out.println(v2.toString());
-        System.out.println("v1.x = "+v1.x);
+        System.out.println("v1.x = " + v1.x);
 
         System.out.println(v1.length());
+        v1.skalarMod(2);
+        System.out.println(v1);
+        Vertex v3 = v1.add(v2);
+        System.out.println(v3);
+        v1.skalarMod(0.5);
+        System.out.println(v1.equals(v2));
+        System.out.println(v1.equals(v4));
 
-    
     }
 }
