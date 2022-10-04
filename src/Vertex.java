54e8ae95c0fccf6d13d8c08659ec346050d151d7
hibbes <marek@czernohous.de>
2022-10-04 07:52:01 +0200

    SimpleTriangle

--- src/Vertex.java
+++ src/Vertex.java
@@ -18,14 +18,43 @@ public class Vertex {
 
     public void skalarMod(double s) {
         this.x = x * s;
-        this.y = y.s;
+        this.y = y * s;
     }
 
     public Vertex skalarMultMod(double s) {
         return new Vertex(x * s, y * s);
     }
 
+    public Vertex add(Vertex v2) {
+        return new Vertex(x + v2.x, y + v2.y);
+    }
+
     public void addMod(Vertex v2) {
         this.x = x + v2.x;
     }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public boolean equals(Object thatObject) {
+        if (thatObject instanceof Vertex) {
+            Vertex that = (Vertex) thatObject;
+            return this.x == that.x && this.y == that.y;
+        }
+        return false;
+    }
+
 }
