b8e5ff5d23ea62be22484e1cc75bc10c5f492eff
Marek Czernohous <marek@czernohous.de>
2022-10-27 13:09:14 +0200

    MovableImage

---
+++ src/GameObject.java
@@ -0,0 +1,19 @@
+public interface GameObject extends Paintable, Movable {
+    double getWidth();
+
+    double getHeight();
+
+    Vertex getPos();
+
+    Vertex getDeltaPos();
+
+    void setDeltaPos(Vertex v);
+
+    boolean isLargerThan(GameObject that);
+
+    boolean isLeftOf(GameObject that);
+
+    boolean isAbove(GameObject that);
+
+    boolean touches(GameObject that);
+}
